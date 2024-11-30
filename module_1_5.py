73a90aae815c5ce40fd828a477393e343492a586
Денис Масляев <d.maslyaev@crpt.ru>
2024-11-30 12:00:01 +0300

    add module_1_5.py

---
+++ module_1_5.py
@@ -0,0 +1,8 @@
+immutable_var=(1,"Moscow",[5,6,"book"])
+print(immutable_var)
+# immutable_var[1] = 2
+print(immutable_var[1])
+mutable_list = [1,"dva",3.5]
+print(mutable_list)
+mutable_list[1] = "string"
+print(mutable_list)
